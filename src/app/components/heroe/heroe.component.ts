79cf98dac8a7f316b287a169f9acb57a6b39a43e
Ruiz Acosta <brenda.ruiz.acosta@accenture.com>
2018-04-03 23:18:10 -0500

    another lesson

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute} from '@angular/router';
+import { HeroesService} from '../../servicios/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
@@ -7,13 +9,24 @@ import { ActivatedRoute} from '@angular/router';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor(private activatedRoute:ActivatedRoute ){
+  heroe:any = {};
+
+  constructor(private activatedRoute:ActivatedRoute,
+              private _heroesService: HeroesService,
+              private _router: Router
+   ){
     this.activatedRoute.params.subscribe( params => {
-      console.log(params);
+      //console.log(params['id']);
+      this.heroe = this._heroesService.getHeroe(params['id']);
+      console.log(this.heroe);
     })
    }
 
   ngOnInit() {
+  }
+
+  backHome(){
+    this._router.navigate(['heroes']);
   }
 
 }
